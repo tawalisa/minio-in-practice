18b179878db22ccb58b498240f58740dcb311c8a
lijia <tawalisa@163.com>
2024-05-09 11:23:51 +0800

    add upload presigned demo

--- src/test/java/allen/example/minio/demo/MinioDemoApplicationTest.java
+++ src/test/java/allen/example/minio/demo/MinioDemoApplicationTest.java
@@ -2,6 +2,7 @@ package allen.example.minio.demo;
 
 import io.minio.*;
 import io.minio.errors.MinioException;
+import io.minio.http.Method;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -87,10 +88,35 @@ class MinioDemoApplicationTest {
 //
 //    }
 //
-//    @Test
-//    void testPreSignedUploadObject(){
-//
-//    }
+    @Test
+    void testPreSignedUploadObject(){
+        try {
+            MinioClient minioClient =
+                    MinioClient.builder()
+                            .endpoint("http://127.0.0.1:9000")
+                            .credentials("admin", "password")
+                            .build();
+
+            String bucketName = "test";
+            String objectName = "upload1.jpg";
+            // Make 'test' bucket if not exist.
+            boolean found =
+                    minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+            if (!found) {
+                // Make a new bucket called 'test'.
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+            } else {
+                System.out.printf("Bucket %s already exists.%n", bucketName);
+            }
+            String preSignedURL = minioClient.getPresignedObjectUrl(GetPresignedObjectUrlArgs.builder().bucket(bucketName).method(Method.PUT)
+                    .object(objectName).expiry(24 * 60 * 60).build());
+            System.out.printf("successfully download as object '%s' to bucket '%s'. preSignedURL -> %s", objectName, bucketName, preSignedURL);
+        } catch (MinioException e) {
+            log.error("{} {}", e.getMessage(), e.httpTrace(), e);
+        } catch (IOException | NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+    }
 //
 //    @Test
 //    void testPreSignedDownloadObject(){
